b6e81e6c901eb4b246c1a580a194de3b3706005c
magical-flow-3186 <rregalado17@gmail.com>
2019-06-01 21:00:44 +0000

    Automatically backed up by Learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,9 +1,8 @@
 def using_until
         levitation_force = 6
-  until levitation_force < 11
-  puts "Windardium Leviosa"
+  until levitation_force == 10
     levitation_force += 1
 end
-    
+puts "Wingardium Leviosa"
 end
 
